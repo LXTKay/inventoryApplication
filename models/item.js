3ed7dd4846163c84e6663c0b0f9aa41779cd992a
lxtk <thealextk@gmail.com>
2024-05-16 20:51:46 +0200

    test population of database

--- models/item.js
+++ models/item.js
@@ -6,12 +6,13 @@ const ItemSchema = new Schema({
   name: {type: String, required: true, maxLength: 100, minLength: 1},
   description: {type: String, required: true, maxLength: 500},
   price: {type: Number, required: true},
-  inStock: {type: Number, required: true},
-  category: {type: Schema.Types.ObjectId, ref: "Category"}
+  amount: {type: Number, required: true},
+  category: {type: Schema.Types.ObjectId, ref: "Category", required: true},
+  picture: {type: String}
 });
 
 ItemSchema.virtual("url").get(function(){
   return "/item/" + this._id;
 })
 
-module.exports = mongoose.model("Category", CategorySchema);
+module.exports = mongoose.model("Item", ItemSchema);
---
+++ populateDB.js
@@ -0,0 +1,93 @@
+#! /usr/bin/env node
+
+console.log(
+  'This script populates some test items and category to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+);
+
+// Get arguments passed on command line
+const userArgs = process.argv.slice(2);
+
+const Item = require("./models/item");
+const Category = require("./models/category");
+
+const items = [];
+const categories = [];
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const mongoDB = userArgs[0];
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  console.log("Debug: About to connect");
+  await mongoose.connect(mongoDB);
+  console.log("Debug: Should be connected?");
+  await createCategories();
+  await createItems();
+  console.log("Debug: Closing mongoose");
+  mongoose.connection.close();
+}
+
+// We pass the index to the ...Create functions so that, for example,
+// genre[0] will always be the Fantasy genre, regardless of the order
+// in which the elements of promise.all's argument complete.
+
+async function itemCreate(index, name, description, price, amount, category){
+  const item = new Item({
+    name: name,
+    description: description,
+    price: price,
+    amount: amount,
+    category: category
+  });
+  await item.save();
+  items[index] = item;
+  console.log("Added Item: " + name)
+};
+
+async function categoryCreate(index, name, description){
+  const category = new Category({
+    name: name,
+    description: description
+  });
+  await category.save();
+  categories[index] = category;
+  console.log("Added Category: " + name);
+};
+
+async function createCategories(){
+  console.log("Adding Categories!");
+  await Promise.all([
+    categoryCreate(0, "Seating Accommodations", "Things the customer can sit on."),
+    categoryCreate(1, "Table", "Furniture to put things on."),
+    categoryCreate(2, "Beds", "Furniture to sleep in."),
+    categoryCreate(3, "Cupboards", "Furniture to store the customers inventory."),
+  ])
+};
+
+async function createItems(){
+  console.log("Adding Items!");
+  await Promise.all([
+    itemCreate(0, "Nice Stool", "A nice stool made from wood.", 20, 22, categories[0]),
+    itemCreate(1, "White Couch", "A big white couch.", 500, 3, categories[0]),
+    itemCreate(2, "Dynamic Office Chair", "An office chair that is nice for your back.", 80, 10, categories[0]),
+    itemCreate(3, "Regular Chair", "A regular chair.", 49.99, 40, categories[0]),
+
+    itemCreate(4, "Super Table", "High tech table with lots of settings.", 400, 2, categories[1]),
+    itemCreate(5, "Dining Table", "A table to eat from.", 150, 31, categories[1]),
+    itemCreate(6, "Office Table", "A small table for the office.", 119.99, 100, categories[1]),
+
+    itemCreate(7, "Awesome Bed", "A bed for the bedroom.", 150, 2, categories[2]),
+    itemCreate(8, "Big Bed Mega XXL", "A giant bed everyone wants to sleep in.", 1299, 1, categories[2]),
+    itemCreate(9, "Moderate Bed Green", "A good bed for poor people.", 300, 12, categories[2]),
+    itemCreate(10, "Jail Bed", "Not everyones taste, but at least it is sturdy.", 99, 25, categories[2]),
+
+    itemCreate(11, "Wardrobe", "A cupboard to store your clothes.", 200, 10, categories[3]),
+    itemCreate(12, "Kitchen Cabinet", "A cabinet for tableware.", 180, 10, categories[3]),
+    itemCreate(13, "Giant Cupboard", "A big cupboard to store big things.", 620, 99, categories[3]),
+  ])
+};
+
+//execute: node populateDB <your MongoDB url>
